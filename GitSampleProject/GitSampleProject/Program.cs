77a95e2fd34fad216f8e7c2bd22fb26cff41efad
ashutoshchittora <ashutosh.chittora@asos.com>
2018-02-28 12:57:37 +0000

    added feature 1 new branch

--- GitSampleProject/GitSampleProject/Program.cs
+++ GitSampleProject/GitSampleProject/Program.cs
@@ -13,6 +13,7 @@ namespace GitSampleProject
             Console.WriteLine("Hello");
 
             // changre from VS and commit from VS ...
+            // code to call feature 1
         }
     }
 }
